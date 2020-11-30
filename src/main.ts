fe969983d3ba2ff5225685deeb3967e9243613ce
Mengh <hilliamtung@gmail.com>
2020-11-30 19:26:20 +0000

    Added minHeight and minWidth to browser window

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,8 @@ function _createWindow(): void {
     const mainWindow = new BrowserWindow({
         height: 600,
         width: 800,
+        minHeight: 600,
+        minWidth: 800,
         frame: false,
 
         webPreferences: { // Allows us to load some NodeJS scripts before browser loads
