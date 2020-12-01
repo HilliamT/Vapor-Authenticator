0fc2e78aa73bca931d802e79c4abd59fcf0894b5
Mengh <hilliamtung@gmail.com>
2020-12-01 02:30:45 +0000

    Implemented auth code generation and displaying for a user every 30 seconds

--- src/app/components/AuthSetup.tsx
+++ src/app/components/AuthSetup.tsx
@@ -1,9 +1,27 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 export default function AuthSetup(props) {
     const [receivedSMS, setReceivedSMS] = useState(false); // Capture if user has been sent a SMS to begin authentication
     const [SMSCode, setSMSCode] = useState(""); // Input for user's SMS response
-    const { setupDesktopAuth, finishDesktopAuth, revokeDesktopAuth } = window["electron"].authenticate;
+    const [authCode, setAuthCode] = useState("");
+    const [seconds, setSeconds] = useState(new Date().getSeconds() % 30);
+    const { setupDesktopAuth, finishDesktopAuth, revokeDesktopAuth, getAuthCode } = window["electron"].authenticate;
+
+    // Establish authentication code loop upon context / page switch
+    useEffect(() => {
+        function generateNewAuthCode() {
+            if (!props.user.usingVapor) return;
+            const currentDateSeconds = new Date().getSeconds();
+            let interval;
+            if (currentDateSeconds % 30 == 0) interval = setInterval(() => setAuthCode(getAuthCode()), 30 * 1000);
+            if (currentDateSeconds % 30 != 0) interval = setTimeout(generateNewAuthCode, (30 - currentDateSeconds % 30) * 1000);
+            setAuthCode(getAuthCode());
+            return () => clearInterval(interval);
+        }
+        generateNewAuthCode();
+        setInterval(() => setSeconds(new Date().getSeconds() % 30), 1000);
+    }, [props.user]);
+    
 
     return (<div className="p-4">
 
@@ -29,6 +47,15 @@ export default function AuthSetup(props) {
         {/* This user is using Vapor! */}
         {props.user.usingVapor && <div>
             <div>You are now using Vapor!</div>
+            <br />
+            <div>
+                Your auth code is:
+                <span className="font-bold">{authCode}</span>
+                <br />
+                {30 - seconds}s left
+            </div>
+            <br />
+            
             <div onClick={async () => {
                 const response = await revokeDesktopAuth();
                 ((response.error == null) ? props.updateUser() : "");
