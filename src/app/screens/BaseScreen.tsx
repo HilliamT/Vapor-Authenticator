9f709f59a53b0ddf3f601338512f88e4b6018111
Mengh <hilliamtung@gmail.com>
2020-12-01 18:10:34 +0000

    Top bar of accounts is scrollable now

--- src/app/screens/BaseScreen.tsx
+++ src/app/screens/BaseScreen.tsx
@@ -17,7 +17,7 @@ export default function BaseScreen(props) {
     function renderAccounts() {
         const elems = [];
         for (const account_name in accounts) {
-            elems.push(<div className={`h-20 w-20 flex cursor-pointer hover:bg-black hover:bg-opacity-20 ${(props.user.steamid == accounts[account_name].steamid) ? "bg-black bg-opacity-20" : ""}`}
+            elems.push(<div className={`h-20 w-20 flex-none flex cursor-pointer hover:bg-black hover:bg-opacity-20 ${(props.user.steamid == accounts[account_name].steamid) ? "bg-black bg-opacity-20" : ""}`}
                 key={account_name} onClick={async () => {
                     await window["electron"].setCurrentUser(account_name);
                     props.updateUser();
@@ -25,7 +25,7 @@ export default function BaseScreen(props) {
                     <img className="m-auto h-16 w-16 rounded" src={getAvatarURL(accounts[account_name].avatarHash)}/>
             </div>);
         }
-        elems.push(<div className="h-20 w-20 flex cursor-pointer hover:bg-black hover:bg-opacity-20"
+        elems.push(<div className="h-20 w-20 flex-none flex cursor-pointer hover:bg-black hover:bg-opacity-20"
         key="add" onClick={() => {
             props.setAddNewAccount(true);
         }}>
@@ -38,10 +38,10 @@ export default function BaseScreen(props) {
     }
 
     return (<div>
-        <div className="flex w-full">
+        <div className="w-screen flex">
             <HashRouter>
                 {/* Leading Sidebar */}
-                <div id="Sidebar" className="w-20 h-screen bg-indigo-500 flex flex-wrap content-start">
+                <div id="Sidebar" className="w-20 h-screen bg-indigo-500 flex flex-wrap content-start flex-none">
                     <div className="w-full h-20"></div>
                     <Link className="text-sm w-full text-center h-20 group" to="/">
                         <FontAwesomeIcon icon={faHome} size="2x" className="opacity-30 group-hover:opacity-80"/>
@@ -60,28 +60,28 @@ export default function BaseScreen(props) {
                     </Link>
                 </div>
 
-                <div id="Main" className="flex flex-grow flex-wrap">
-                    {/* Secondary Sidebar */}
-                    <div id="Topbar" className="w-full h-20 bg-indigo-400 flex">
-                        {renderAccounts()}
+                <div id="Main" className="w-auto overflow-hidden h-screen flex flex-wrap">
+                        {/* Secondary Sidebar */}
+                        <div id="Topbar" className="w-full h-20 bg-indigo-400 flex overflow-x-scroll whitespace-nowrap">
+                            {renderAccounts()}
+                        </div>
+                        <div id="MainContent" className="w-full h-full bg-gray-100">
+                            <Switch>
+                                <Route exact path="/">
+                                    <Home />
+                                </Route>
+                                <Route path="/offers/incoming">
+                                    <IncomingTradeOffers user={props.user}/>
+                                </Route>
+                                <Route path="/confirmations">
+                                    <Confirmations user={props.user} />
+                                </Route>
+                                <Route path="/authenticator">
+                                    <AuthSetup user={props.user} updateUser={props.updateUser} />
+                                </Route>
+                            </Switch>
+                        </div>
                     </div>
-                    <div id="MainContent" className="w-full h-full bg-gray-100">
-                        <Switch>
-                            <Route exact path="/">
-                                <Home />
-                            </Route>
-                            <Route path="/offers/incoming">
-                                <IncomingTradeOffers user={props.user}/>
-                            </Route>
-                            <Route path="/confirmations">
-                                <Confirmations user={props.user} />
-                            </Route>
-                            <Route path="/authenticator">
-                                <AuthSetup user={props.user} updateUser={props.updateUser} />
-                            </Route>
-                        </Switch>
-                    </div>
-                </div>
             </HashRouter>
         </div>
     </div>);
