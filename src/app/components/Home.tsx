819c65d997ee77403338b8f4a8c3e34e5c58c94b
Mengh <hilliamtung@gmail.com>
2020-12-03 18:08:17 +0000

    When switching users, SteamUser performs an account switch so old current user has to end idling session - this is updated on the interface

--- src/app/components/Home.tsx
+++ src/app/components/Home.tsx
@@ -1,8 +1,12 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Logo from "../screens/vapor.svg";
 
 export default function Home(props) {
-    const [playingGames, setPlayingGames] = useState(false); 
+    const [playingGames, setPlayingGames] = useState(false);
+
+    useEffect(() => {
+        setPlayingGames(false);
+    }, [props.user]);
 
     return (<div className="m-2 flex flex-wrap">
         <div className="mx-4 font-bold text-2xl w-full">Home</div>
