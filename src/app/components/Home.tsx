a2af7d847a5358897237b15a5014017e6b205271
Mengh <hilliamtung@gmail.com>
2020-12-03 17:53:27 +0000

    Added additional comments to home screen to label buttons

--- src/app/components/Home.tsx
+++ src/app/components/Home.tsx
@@ -9,10 +9,13 @@ export default function Home(props) {
         <div className="m-4 mt-2 p-4 rounded bg-white shadow flex w-full justify-center">
             <img src={Logo} className="h-36 w-36" />
         </div>
+
+        {/* Steam Profile Button */}
         <div className="m-2 ml-4 p-2 rounded bg-white shadow font-bold cursor-pointer" onClick={() => {
             window["electron"].window.loadURL(`https://steamcommunity.com/profiles/${props.user.steamid}`);
         }}>Profile</div>
 
+        {/* Idle games button */}
         <div className={`m-2 ml-4 p-2 rounded bg-white shadow font-bold cursor-pointer ${playingGames ? "bg-yellow-500" : "bg-green-500"}`} onClick={() => {
             window["electron"].currentUser.playGames((playingGames) ? [] : null).then(() => {
                 setPlayingGames(!playingGames);
