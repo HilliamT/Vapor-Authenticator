e9bf689cd98ebed6d32953eedc23177594979c43
Mengh <hilliamtung@gmail.com>
2020-12-10 20:25:02 +0000

    Scaled down size of AccountScreen elements

--- src/app/screens/AccountScreen.tsx
+++ src/app/screens/AccountScreen.tsx
@@ -13,7 +13,7 @@ export default function AccountScreen(props) {
     function renderAccounts() {
         const elems = [];
         for (const account_name in accounts) {
-            elems.push(<div className={`h-32 w-32 cursor-pointer hover:bg-black hover:bg-opacity-20 m-3 inline-block`}
+            elems.push(<div className={`h-24 w-24 cursor-pointer hover:bg-black hover:bg-opacity-20 m-3 inline-block`}
                 key={account_name} onClick={() => {
                     setSwitchingUser(true);
                     window["electron"].setCurrentUser(account_name).then(() => {
@@ -21,16 +21,16 @@ export default function AccountScreen(props) {
                         props.setSwitchingAccounts(false);
                     });
                 }}>
-                    <img className="m-auto h-32 w-32 rounded" src={getAvatarURL(accounts[account_name].avatarHash)}/>
+                    <img className="m-auto h-24 w-24 rounded" src={getAvatarURL(accounts[account_name].avatarHash)}/>
             </div>);
         }
 
-        elems.push(<div className="h-32 w-32 cursor-pointer hover:bg-black hover:bg-opacity-50 m-3 inline-block"
+        elems.push(<div className="h-24 w-24 cursor-pointer hover:bg-black hover:bg-opacity-50 m-3 inline-block"
             key="add" onClick={() => {
                 props.setAddNewAccount(true);
                 props.setSwitchingAccounts(false);
             }}>
-                <div className="float-left h-32 w-32 rounded bg-black bg-opacity-10 justify-center content-center flex">
+                <div className="float-left h-24 w-24 rounded bg-black bg-opacity-10 justify-center content-center flex">
                     <FontAwesomeIcon icon={faPlus} className="m-auto h-16 w-16" color="white" opacity="20%" />
                 </div>
             </div>);
@@ -40,9 +40,9 @@ export default function AccountScreen(props) {
 
     return (<div className="fixed h-screen w-screen z-20">
         {switchingUser && <div className="absolute w-full h-full bg-black bg-opacity-20 z-10"></div>}
-        <div style={{backgroundColor: "#0e0d1c"}} className="p-12 justify-center items-start h-screen w-screen">
+        <div style={{backgroundColor: "#0e0d1c"}} className="p-20 justify-center items-start h-screen w-screen">
             
-            <div id="ScreenTitle" className="text-6xl font-bold text-white my-3">Which account<br/> will you choose today?</div>
+            <div id="ScreenTitle" className="text-4xl font-bold text-white my-3">Which account<br/> will you choose today?</div>
             <br />
             <div id="ScreenContents" className="overflow-y-auto h-full">
                 {renderAccounts()}
